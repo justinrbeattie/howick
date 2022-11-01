a125826430ef8435828e2502bc61d8f14f2bd391
justinrbeattie <justin.r.beattie@gmail.com>
2022-11-01 15:46:46 +1100

    remove details

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,20 +3,11 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
-    <div style="width:100vw;">
+
       <builder-component model="page" api-key="f5a098163c3741e19503f02a69360619">
         Loading...
       </builder-component>
-      <details is="details-extended">
-        <summary>
-          <i class="las la-angle-right toggle"></i>
-          <div>Will my child's 504 Plan be implementedo?</div>
-        </summary>
-        <div>
-          Yes. Similar to the Spring, case managers will reach out to students.
-        </div>
-      </details>
-    </div>
+
 
 
   );
