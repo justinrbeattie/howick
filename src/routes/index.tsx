70c6466e0a4aee13118986ebbb4a631f08a97cbd
justinrbeattie <justin.r.beattie@gmail.com>
2022-11-15 18:11:41 +1100

    builder

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from '@builder.io/qwik';
+/* import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
@@ -18,4 +18,41 @@ export default component$(() => {
 
 export const head: DocumentHead = {
   title: 'Welcome to Qwik',
-};
+}; */
+
+import { component$, Resource, useResource$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
+import { getContent, RenderContent, getBuilderSearchParams } from "@builder.io/sdk-qwik";
+
+export const BUILDER_PUBLIC_API_KEY = "f5a098163c3741e19503f02a69360619";
+export const BUILDER_MODEL = "page";
+
+
+
+export default component$(() => {
+  const location = useLocation();
+  const builderContentRsrc = useResource$<any>(() => {
+    return getContent({
+      model: BUILDER_MODEL,
+      apiKey: BUILDER_PUBLIC_API_KEY,
+      options: getBuilderSearchParams(location.query),
+      userAttributes: {
+        urlPath: location.pathname || "/",
+      },
+    });
+  });
+
+  return (
+    <Resource
+      value={builderContentRsrc}
+      onPending={() => <div>Loading...</div>}
+      onResolved={(content) => (
+        <RenderContent
+          model={BUILDER_MODEL}
+          content={content}
+          apiKey={BUILDER_PUBLIC_API_KEY}
+        />
+      )}
+    />
+  );
+});
