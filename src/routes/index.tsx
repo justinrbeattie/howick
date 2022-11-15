429459349a80e303fdc236dd67b144fe046d9c84
justinrbeattie <justin.r.beattie@gmail.com>
2022-11-15 17:29:59 +1100

    test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,15 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
-
+    <>
       <builder-component model="page" api-key="f5a098163c3741e19503f02a69360619">
         Loading...
       </builder-component>
 
+      <builder-component model="section" api-key="f5a098163c3741e19503f02a69360619">
+      </builder-component>
 
+    </>
 
   );
 });
